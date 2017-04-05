221622d59fc2aee9121e916f0409c3ba2f7c46ba
Peter Karas <pkaras@onetrust.com>
2017-04-05 03:22:33 +0100

    chore(initilaise repo)

---
+++ src/main/java/karas/peter/App.java
@@ -0,0 +1,13 @@
+package karas.peter;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
